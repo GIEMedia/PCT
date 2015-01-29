3862ff1647fd937c29f9230a2e3643caf895b861
Quan Le <quanla2003@gmail.com>
2015-01-29 16:28:07 +0700

    Finished redoing test page to AngularJS

--- PST.Api/app/spa/api/TestService.js
+++ PST.Api/app/spa/api/TestService.js
@@ -24,45 +24,45 @@
                                 "text": "False"
                             }
                         ]
-                    },
-                    {
-                        "text": "Which of the following is TRUE regarding First Aid should someone accidentally swallow this product?",
-                        "options" : [
-                            {
-                                "text": "Have the person drink some water."
-                            },
-                            {
-                                "text": "Do not induce vomiting."
-                            },
-                            {
-                                "text": "Do not induce vomiting unless instructed to do so by a doctor or poison control center."
-                            },
-                            {
-                                "text": "Induce vomiting."
-                            }
-                        ]
-                    },
-                    {
-                        "text": "In the event of an accident with this product, it is important to have the product label ready to provide to the doctor.",
-                        "options" : [
-                            {
-                                "text": "True"
-                            },
-                            {
-                                "text": "False"
-                            }
-                        ]
-                    },
-                    {
-                        "text": "This product has an antidote.",
-                        "options" : [
-                            {
-                                "text": "True"
-                            },
-                            {
-                                "text": "False"
-                            }
-                        ]
+                    //},
+                    //{
+                    //    "text": "Which of the following is TRUE regarding First Aid should someone accidentally swallow this product?",
+                    //    "options" : [
+                    //        {
+                    //            "text": "Have the person drink some water."
+                    //        },
+                    //        {
+                    //            "text": "Do not induce vomiting."
+                    //        },
+                    //        {
+                    //            "text": "Do not induce vomiting unless instructed to do so by a doctor or poison control center."
+                    //        },
+                    //        {
+                    //            "text": "Induce vomiting."
+                    //        }
+                    //    ]
+                    //},
+                    //{
+                    //    "text": "In the event of an accident with this product, it is important to have the product label ready to provide to the doctor.",
+                    //    "options" : [
+                    //        {
+                    //            "text": "True"
+                    //        },
+                    //        {
+                    //            "text": "False"
+                    //        }
+                    //    ]
+                    //},
+                    //{
+                    //    "text": "This product has an antidote.",
+                    //    "options" : [
+                    //        {
+                    //            "text": "True"
+                    //        },
+                    //        {
+                    //            "text": "False"
+                    //        }
+                    //    ]
                     }
                 ];
             };
@@ -70,12 +70,25 @@
             TestService.submit = function(data, callback) {
                 if (data.round == 1) {
                     return {
+                        passed: false,
                         corrects: [
-                            {},
-                            {}
+                            //{
+                            //    question: 0,
+                            //    explanation: "The signal word is Caution, not Warning."
+                            //},
+                            //{
+                            //    question: 2,
+                            //    explanation: "The label is important for the doctor or poison control center to have. If available, also providing the product's material safety data sheet (MSDS) is helpful."
+                            //}
                         ]
                     };
+                } else if (data.round == 2) {
+                    return {
+                        passed: true
+                    };
                 }
+
+                setTimeout(callback, 0);
             };
 
             return TestService;
--- PST.Api/app/spa/test/Test.js
+++ PST.Api/app/spa/test/Test.js
@@ -20,9 +20,6 @@
         .controller("test.Ctrl", function ($scope, TestService, $state) {
             $scope.round = 1;
 
-            $scope.questions = TestService.query({}, function() {
-                $scope.question = $scope.questions[0];
-            });
 
             $scope.model = {
                 answer: null,
@@ -30,19 +27,53 @@
             };
 
             var acceptAnswer = function() {
-                $scope.model.answers.push($scope.model.answer);
+                $scope.model.answers[$scope.questions.indexOf($scope.question)] = ($scope.model.answer);
                 $scope.model.answer = null;
             };
+
+            var nextFailedQuestion = function(index) {
+                if (index == -1) {
+                    index = 0;
+                }
+                for (;index < $scope.questions.length;index++) {
+                    var question = $scope.questions[index];
+                    if (question.correct == false) {
+                        return question;
+                    }
+                }
+                return null;
+            };
+
+            var fetchNextQuestion = function() {
+                if ($scope.round != 3) {
+                    var index = $scope.questions.indexOf($scope.question);
+                    $scope.question = $scope.questions[index + 1];
+                    if ($scope.model.answers.length > index + 1) {
+                        $scope.model.answer = $scope.model.answers[index + 1];
+                    } else {
+                        $scope.model.answer = null;
+                    }
+                } else {
+                    var index = $scope.questions.indexOf($scope.question);
+                    $scope.question = nextFailedQuestion(index);
+                    $scope.model.answer = null;
+                }
+
+            };
+
+            $scope.questions = TestService.query({}, fetchNextQuestion);
 
             $scope.next = function() {
                 acceptAnswer();
 
-                var index = $scope.questions.indexOf($scope.question);
-                $scope.question = $scope.questions[index + 1];
+                fetchNextQuestion();
             };
 
             $scope.progress = function() {
-                var index = $scope.model.answers.length;
+                if ($scope.question == null) {
+                    return "100%";
+                }
+                var index = $scope.questions.indexOf($scope.question);
 
                 return Math.round(index / $scope.questions.length * 100) + "%";
             };
@@ -54,7 +85,48 @@
             $scope.submit = function() {
                 acceptAnswer();
 
-                $scope.result = TestService.submit({answers: $scope.answers, round: $scope.round});
+                $scope.question = null;
+                $scope.model.answer = null;
+
+                $scope.result = TestService.submit({answers: $scope.answers, round: $scope.round}, function() {
+                    if ($scope.result.passed) {
+                        $state.go("testPass");
+                    }
+                });
+            };
+
+            $scope.nextRound = function() {
+                var setCorrectQuestions = function() {
+
+                    for (var i in $scope.result.corrects) {
+                        if ($scope.result.corrects.hasOwnProperty(i)) {
+
+                            var correct = $scope.result.corrects[i];
+                            var question = $scope.questions[correct.question * 1];
+                            question.correct = true;
+                            question.explanation = correct.explanation;
+                        }
+                    }
+                };
+
+                if ($scope.round == 1) {
+                    for (var i in $scope.questions) {
+                        if ($scope.questions.hasOwnProperty(i)) {
+                            $scope.questions[i].correct = false;
+                        }
+                    }
+                    setCorrectQuestions();
+
+                    $scope.result = null;
+                    fetchNextQuestion();
+                } else if ($scope.round == 2) {
+
+                    setCorrectQuestions();
+
+                    $scope.result = null;
+                    fetchNextQuestion();
+                }
+                $scope.round++;
             };
         })
 
