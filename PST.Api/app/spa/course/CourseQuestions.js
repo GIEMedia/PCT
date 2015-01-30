52e7a367896d3d175f360afdb77f82b2196f345c
Quan Le <quanla2003@gmail.com>
2015-01-30 17:42:33 +0700

    Re-architect course question component to show congrats when finishing single section

--- PST.Api/app/spa/course/CourseQuestions.js
+++ PST.Api/app/spa/course/CourseQuestions.js
@@ -13,58 +13,34 @@
                 link: function($scope, elem, attrs, courseQuestionsContainerCtrl) {
                     var emptyAnswer = function() {
                         return {
-                            checkeds: [],
+                            checks: [],
                             choice: null
                         };
                     };
-                    $scope.answer = emptyAnswer();
-                    $scope.correctAnswer = false;
 
-                    var _questions;
-                    $scope.$watch(attrs.questions, function(questions) {
-                        _questions = questions;
-                        if (questions) {
-                            $scope.question = questions[0];
+                    var _question;
+                    $scope.$watch(attrs.question, function(question) {
+                        if (question) {
+                            $scope.answer = emptyAnswer();
+                            _question = question;
                         }
-
                     });
 
                     $scope.submitAnswer = function() {
-                        if ($scope.question.type == "pictures") {
-                            var answer = [];
-                            for (var i = 0; i < $scope.answer.checkeds.length; i++) {
-                                if ($scope.answer.checkeds[i]) {
+                        var answer;
+                        if (_question.type == "pictures") {
+                            answer = [];
+                            for (var i = 0; i < $scope.answer.checks.length; i++) {
+                                if ($scope.answer.checks[i]) {
                                     answer.push(i);
                                 }
                             }
-                        } else if (["text","picture","video"].indexOf($scope.question.type) > -1) {
-                            var answer = $scope.answer.choice == null ? -1 : $scope.answer.choice * 1;
-                        }
-
-                        if (ObjectUtil.equals(answer, $scope.question.answer)) {
-                            $scope.correctAnswer = true;
-                            courseQuestionsContainerCtrl.showResult(true, $scope.question.explanation);
+                        } else if (["text","picture","video"].indexOf(_question.type) > -1) {
+                            answer = $scope.answer.choice == null ? -1 : $scope.answer.choice * 1;
                         } else {
-                            courseQuestionsContainerCtrl.showResult(false);
+                            throw "Unknown type: " + _question.type;
                         }
-                    };
-
-                    courseQuestionsContainerCtrl.nextQuestion = function() {
-                        $scope.correctAnswer = false;
-                        $scope.answer = emptyAnswer();
-                        courseQuestionsContainerCtrl.showResult(null);
-
-                        var indexOf = _questions.indexOf($scope.question);
-                        if (indexOf > -1) {
-                            $scope.question = _questions[indexOf + 1];
-                            return true;
-                        } else {
-                            return false;
-                        }
-                    };
-
-                    courseQuestionsContainerCtrl.progress = function() {
-                        return _questions == null ? 0 : _questions.indexOf($scope.question);
+                        courseQuestionsContainerCtrl.submitAnswer(answer);
                     };
                 }
             };
--- PST.Api/app/spa/course/CourseQuestionsContainer.js
+++ PST.Api/app/spa/course/CourseQuestionsContainer.js
@@ -9,44 +9,78 @@
             return {
                 restrict: "C",
                 require: "^course",
+                scope: true,
                 templateUrl: "/app/spa/course/CourseQuestionsContainer.html",
                 link: function($scope, elem, attrs, courseCtrl) {
-                    $scope.$watch(attrs.section, function(section) {
-                        $scope.section = section;
+
+                    var ladyUserProgress = Async.ladyFirst();
+                    $scope.$watch("userProgress", function(progress) {
+                        if (progress) {
+                            ladyUserProgress.ladyDone();
+                        }
                     });
-                    $scope.nextSection = function() {
-                        courseCtrl.nextSection();
-                    };
+
+                    $scope.$watch(attrs.section, function(section) {
+                        $scope.result = null;
+
+                        ladyUserProgress.manTurn(function() {
+                            var sectionNum = courseCtrl.sectionNum();
+                            var progress = $scope.userProgress.sections[sectionNum - 1];
+                            $scope.question = section.questions[progress];
+                            
+                            if (!$scope.$$phase) $scope.$digest();
+                        });
+                    });
+                    //$scope.nextSection = function() {
+                    //    courseCtrl.nextSection();
+                    //};
                     $scope.sectionNum = function() {
                         return courseCtrl.sectionNum();
                     };
-                },
-                controller: function($scope) {
-                    $scope.result = null;
 
-                    var ctrl = this;
-                    ctrl.nextQuestion = null;
-                    ctrl.progress = null;
-
-                    $scope.progress = function() {
-                        return ctrl.progress ? ctrl.progress() : 0;
-                    };
                     $scope.nextQuestion = function() {
-                        var showingNextQuestion = ctrl.nextQuestion();
-                        if (showingNextQuestion) {
-                            ;
+                        $scope.result = null;
+
+                        var indexOf = $scope.section.questions.indexOf($scope.question);
+                        if (indexOf < $scope.section.questions.length - 1) {
+                            $scope.question = $scope.section.questions[indexOf + 1];
+
+                            // Update progress
+                            var sectionNum = courseCtrl.sectionNum();
+                            $scope.userProgress.sections[sectionNum - 1] = indexOf + 1;
                         } else {
-                            $scope.nextSection();
+                            // TODO
+                            //return false;
                         }
+
                         return false;
                     };
 
-                    ctrl.showResult = function(passed, explanation) {
+
+                    $scope.progress = function() {
+                        return $scope.section == null ? 0 : $scope.section.questions.indexOf($scope.question);
+                    };
+
+                },
+                controller: function($scope) {
+                    var ctrl = this;
+
+                    ctrl.submitAnswer = function(answer) {
+
+                        if (ObjectUtil.equals(answer, $scope.question.answer)) {
+                            showResult(true, $scope.question.explanation);
+                        } else {
+                            showResult(false);
+                        }
+
+                        if (!$scope.$$phase) $scope.$digest();
+                    };
+
+                    var showResult = function(passed, explanation) {
                         $scope.result = passed == null ? null : {
                             passed: passed,
                             explanation: explanation
                         };
-                        if (!$scope.$$phase) $scope.$digest();
                     };
                 }
             };
