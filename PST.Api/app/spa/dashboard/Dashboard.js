817df45ea01b5d3a757b3604bf1ee3c8bb27361d
Quan Le <quanla2003@gmail.com>
2015-02-03 23:38:06 +0700

    Dashboard page: with search functionality

--- PST.Api/app/spa/dashboard/Dashboard.js
+++ PST.Api/app/spa/dashboard/Dashboard.js
@@ -29,18 +29,52 @@
             };
 
             $scope.$watch("view.search", function(search) {
-                $scope.courseHeaderCols = DashboardHelper.toCols(DashboardHelper.filter(_courseStructure));
+                $scope.courseHeaderCols = DashboardHelper.toCols(DashboardHelper.filter(_courseStructure, search));
             });
         })
 
         .factory("DashboardHelper", function() {
-
             return {
-                filter: function(headers) {
+                filter: function(_headers, search) {
+                    if (StringUtil.isBlank(search)) {
+                        return _headers;
+                    }
+                    var lowerSearch = search.toLowerCase();
+
+                    var filterCat = function(cat) {
+                        var courses = Cols.filter(cat.courses, function(course) {
+                            return course.name.toLowerCase().indexOf(lowerSearch) > -1;
+                        });
+                        if (Cols.isEmpty(courses)) {
+                            return null;
+                        }
+
+                        return {name: cat.name, courses: courses};
+                    };
+
+
+                    var headers = [];
+                    for (var i = 0; i < _headers.length; i++) {
+                        var categories = Cols.yield(_headers[i].categories, filterCat);
+
+                        if (Cols.isEmpty(categories)) {
+                            continue;
+                        }
+
+                        headers.push({name: _headers[i].name, categories: categories});
+                    }
                     return headers;
                 },
                 toCols: function(list) {
-                    return [[list[0]], [list[1]]];
+                    if (Cols.isEmpty(list)) {
+                        return null;
+                    }
+                    if (list.length == 1) {
+                        return [[list[0]]];
+                    }
+
+                    var middle = Math.floor(list.length / 2);
+                    return [list.slice(0, middle), list.slice(middle, list.length)];
                 }
             };
         })
