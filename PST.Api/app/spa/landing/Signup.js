af31d1b3a6b9b1d970733a5a166a606e71f3e671
Quan Le <quanla2003@gmail.com>
2015-02-04 20:53:05 +0700

    Signup showing error messages

--- PST.Api/app/spa/landing/Signup.js
+++ PST.Api/app/spa/landing/Signup.js
@@ -4,7 +4,7 @@
 
     angular.module('pct.elearning.signup', [
     ])
-        .directive("signUp", function(AccountService, StateService) {
+        .directive("signUp", function(AccountService, StateService, SecurityService, $state) {
             return {
                 restrict: "E",
                 templateUrl: "/app/spa/landing/Signup.html",
@@ -13,7 +13,23 @@
 
                     $scope.getStarted = function() {
 
-                        AccountService.createAccount($scope.signup);
+                        AccountService.createAccount($scope.signup)
+                            .success(function() {
+                                SecurityService.login({
+                                    grant_type: "password",
+                                    username: $scope.signup.email,
+                                    password: $scope.signup.password
+                                })
+                                    .success(function() {
+                                        $state.go("dashboard");
+                                    })
+                                ;
+                            })
+                            .error(function(data) {
+                                //{"Message":"The request is invalid.","ModelState":{"":["Account with this UserName already exists"]}}
+                                alert(JSON.stringify(data));
+                            })
+                        ;
                         //$state.go("dashboard");
                     };
 
