1e6d7397e7c44d5e0a79f6f5691712501f4e6d98
Quan Le <quanla2003@gmail.com>
2015-03-19 00:45:40 +0700

    Prepare Section API for inserting

--- PST.API/Areas/Management/app/spa/api/SectionService.js
+++ PST.API/Areas/Management/app/spa/api/SectionService.js
@@ -12,6 +12,20 @@
                 },
                 setTitle: function(newTitle, courseID, sectionID) {
                     return Api.put("api/manage/course/section/rename/" + courseID + "/" + sectionID, JSON.stringify(newTitle));
+                },
+                upsert: function(section, courseID) {
+                    return Api.put("api/manage/course/section/" + courseID, section);
+                },
+                setOrder: function(order, courseID) {
+                    //order = [
+                    //    "1d81d18e-174c-4323-935b-ff41db4e18ac",
+                    //    "67cf4733-801b-4c48-9b57-fdaa06cc0686",
+                    //    "3e1fde52-960b-4302-bf12-eb9bcf0ee94f"
+                    //]
+                    return Api.put("api/manage/course/section/sort/" + courseID, order);
+                },
+                delete: function(courseID, sectionID) {
+                    return Api.delete("api/manage/course/section/" + courseID + "/" + sectionID);
                 }
             };
         })
