e78445dbe45db3147ca1ca8ceafbcaad87e35d44
Le Anh Quan <quanla2003@gmail.com>
2015-03-25 05:11:18 +0700

    Prepare minify

--- PST.Api/app/spa/app.js
+++ PST.Api/app/spa/app.js
@@ -32,11 +32,11 @@
             'ngResource'
     ])
 
-        .run(function (Api) {
+        .run(["Api", function (Api) {
             // For development
 //            Api.setHost("localhost:53130");
             Api.setHost("gie-test.prototype1.io");
-        })
+        }])
 
 
         .run(["$rootScope", "$state", "$stateParams", function ($rootScope, $state, $stateParams) {
