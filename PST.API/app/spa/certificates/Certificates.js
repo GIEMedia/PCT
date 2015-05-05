4fee23aa6f23657d605136173a5343a784b3f3e5
Le Anh Quan <LeAnhQuan@prototype1.io>
2015-05-05 14:26:45 +0700

    PCT Consumer: Dashboard has new section for recently Earned Certificates

--- PST.API/app/spa/certificates/Certificates.js
+++ PST.API/app/spa/certificates/Certificates.js
@@ -9,16 +9,34 @@
             $stateProvider
                 .state('certificates', {
                     url: '/certificates',
-                    templateUrl: "/app/spa/certificates/Certificates.html",
-                    controller: "certificates.Ctrl"
+                    templateUrl: "/app/spa/certificates/CertificatesPage.html"
                 })
             ;
         }])
         
-        .controller("certificates.Ctrl", ["$scope", "CertificateService", function ($scope, CertificateService) {
-            CertificateService.getEarnedCertificates().success(function(certificates) {
-                $scope.earnedCertificates = certificates;
-            });
+        .directive("pctCertificates", ["CertificateService", function(CertificateService) {
+            return {
+                restrict: "A",
+                scope: true,
+                templateUrl: "/app/spa/certificates/Certificates.html",
+                link: function($scope, elem, attrs) {
+                    $scope.viewConfig = $scope.$eval(attrs.pctCertificates);
+
+                    if ($scope.viewConfig.hideOnEmpty) {
+                        $scope.$watch("(earnedCertificates.length || 0) > 0", function(hasData) {
+                            if (hasData) {
+                                elem.show();
+                            } else {
+                                elem.hide();
+                            }
+                        });
+                    }
+
+                    CertificateService.getEarnedCertificates().success(function(certificates) {
+                        $scope.earnedCertificates = certificates;
+                    });
+                }
+            };
         }])
     ;
 
